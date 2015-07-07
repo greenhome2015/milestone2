02c663559906e61de7120d8da3972a30b03c3257
Saen7 <sven.motschnig@t-online.de>
2015-07-07 14:33:41 +0200

    Fabi Update: index.html und style.css

---
+++ JSON/results.php
@@ -0,0 +1,62 @@
+[
+{
+"Picture" : "",
+"Fabricator" : "Sumsang 40" UE12345LCD",
+"Saving" : "15€ / Monat",
+"Price" : "499",
+},
+{
+"Picture" : "",
+"Fabricator" : "Sunny Bavaria 40" KDL15C ",
+"Saving" : "12€/Monat",
+"Price" : "475",
+},
+{
+"Picture" : "",
+"Fabricator" : "Funkentele 40" 123ZX",
+"Saving" : "11€ / Monat",
+"Price" : "375€",
+},
+{
+"Picture" : "",
+"Fabricator" : "Olufson Bang 40" Prestige ",
+"Saving" : "8€ / Monat",
+"Price" : "590€",
+},
+{
+"Picture" : "",
+"Fabricator" : "Mediom ASD40",
+"Saving" : "8€ / Monat",
+"Price" : "250€",
+},
+{
+"Picture" : "",
+"Fabricator" : "",
+"Saving" : "",
+"Price" : "",
+},
+{
+"Picture" : "",
+"Fabricator" : "",
+"Saving" : "",
+"Price" : "",
+},
+{
+"Picture" : "",
+"Fabricator" : "",
+"Saving" : "",
+"Price" : "",
+},
+{
+"Picture" : "",
+"Fabricator" : "",
+"Saving" : "",
+"Price" : "",
+},
+{
+"Picture" : "",
+"Fabricator" : "",
+"Saving" : "",
+"Price" : "",
+},
+]
